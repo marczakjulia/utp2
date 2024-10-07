33cc0cd5ceb4b7cf4b45d51de25199bd3e20f34a
s31098 <s31098@pjwstk.edu.pl>
2024-10-07 09:26:09 +0200

    Cierpienia młodego wertera

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,9 @@ public class Main {
         System.out.println(adder.add(1, 2));
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
+
     }
 }
+
+// Kocham PJATK
+// Bardzo chce do domu bardzo i nie chce jutro isc do pracy ani po jutrze na uczelnie
