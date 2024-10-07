9511c8d48232d0fa2a94fd61ad4dc5b9269096ae
marczakjulia <s31153@pjwstk.edu.pl>
2024-10-07 09:29:51 +0200

    nie chce mi sie

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println(adder.add(1, 2));
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
+        // umieram pawel zabij mnie prosze
     }
 }
